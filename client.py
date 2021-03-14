ebecf73ae35e5587e155da1bd79956704f0ac809
Shirlyn Tang <shirlyntang@gmail.com>
2021-03-13 19:13:04 -0800

    added client get/put/leader messages and threading to check for response

--- client.py
+++ client.py
@@ -17,6 +17,7 @@ def connect():
 	print("connected to servers!")
 
 def cmd_input():
+	global response_received
 	while True:
 		try:
 			#if input is connect, connect to other clients
@@ -28,21 +29,25 @@ def cmd_input():
 			#if input is send, send a message to a server
 			elif inp[0:4] == 'send':
 				which_server = inp[5:12]
-				message = inp[13:]
-				print("sending message: " + message + ", from client " + str(process_id) + " to " + which_server)
-				message = "client" + str(process_id) + " " + message
-				message = message.encode()
-				time.sleep(5)
-				if (which_server == 'server1'):
-					server1.sendall(message)
-				elif (which_server == 'server2'):
-					server2.sendall(message)
-				elif (which_server == 'server3'):
-					server3.sendall(message)
-				elif (which_server == 'server4'):
-					server4.sendall(message)
-				elif (which_server == 'server5'):
-					server5.sendall(message)
+				operation = inp[13:inp.find(' ', 13)]
+				response_received = False
+
+				if operation == 'leader':
+					send_leader_request()
+				#print("sending message: " + message + ", from client " + str(process_id) + " to " + which_server)
+				elif operation == 'get':
+					message = inp[13:]
+					message = "client" + str(process_id) + " " + message
+					time.sleep(5)
+					send_get_request(message)
+				elif operation == 'put':
+					message = inp[13:]
+					message = "client" + str(process_id) + " " + message
+					time.sleep(5)
+					send_put_request(message)
+				else:
+					print("not a valid option")
+					break
 
 			#if input is exit, close everything
 			elif inp == 'exit':
@@ -56,6 +61,85 @@ def cmd_input():
 		except EOFError:
 			pass
 
+def send_get_request(message):
+	global response_received
+	print("sending get request: ", message)
+	#message = "get " + key
+	message_bytes = message.encode()
+	current_server.sendall(message_bytes)
+
+	threading.Thread(target=check_if_response).start()
+	recv_msg_bytes = current_server.recv(1024)
+	if not recv_msg_bytes:
+		return
+	response_received = True
+	recv_msg = recv_msg_bytes.decode()
+	if recv_msg == "NO_KEY": #todo: print out results for all of these?
+		return
+	else:
+		return #todo: handle when client receives a value
+
+
+def send_put_request(message):
+	global response_received
+	#print("sending put request: key:", key, ", value: ", value)
+	print("sending put request: ", message)
+	#message = "put " + key + " " + value
+	message_bytes = message.encode()
+	current_server.sendall(message_bytes)
+
+	threading.Thread(target=check_if_response).start()
+	recv_msg_bytes = current_server.recv(1024)
+	if not recv_msg_bytes:
+		return
+	response_received = True
+	recv_msg = recv_msg_bytes.decode()
+	if recv_msg == "ack":
+		return
+
+def send_leader_request():
+	global response_received
+	print("sending leader request")
+	message = "leader"
+	message_bytes = message.encode()
+	current_server.sendall(message_bytes)
+
+	threading.Thread(target=check_if_response).start()
+	recv_msg_bytes = current_server.recv(1024)
+	if not recv_msg_bytes:
+		return
+	response_received = True
+	recv_msg = recv_msg_bytes.decode()
+	if recv_msg == "ok": #todo: change this to what's sent when new leader is determined
+		return
+
+def switch_servers():
+	print("switching servers")
+	global current_server
+	if current_server == server1:
+		current_server = server2
+	elif current_server == server2:
+		current_server = server3
+	elif current_server == server3:
+		current_server = server4
+	elif current_server == server4:
+		current_server = server5
+	elif current_server == server5:
+		current_server = server1
+
+def check_if_response():
+	time.sleep(20) #todo: change timeout time if needed
+	if not response_received:
+		handle_no_response()
+
+def handle_no_response():
+	print("no response from current server. switching servers and sending leader request")
+	switch_servers()
+	send_leader_request()
+
+#todo: do i have to do the faillink stuff and print blockchain in the client?
+
+
 #ask tomorrow if we should expect "connect" input to connect to servers
 process_id = int(sys.argv[1])
 
@@ -67,5 +151,8 @@ server2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server5 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+current_server = server1
+response_received = False
 
 threading.Thread(target=cmd_input).start()
